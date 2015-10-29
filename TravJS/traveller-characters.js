fd050bfaf8dad4a26eca5cad088342421abd3fb9
Jared <Jared_August@selinc.com>
2015-10-29 09:19:57 -0700

    Saved character in a service so it will persist as long as the page is loaded

--- TravJS/traveller-characters.js
+++ TravJS/traveller-characters.js
@@ -1,10 +1,14 @@
-charModule = angular.module('travellerCharacters', []) //declare the module for handling chracters
+charModule = angular.module('travellerCharacters', []); //declare the module for handling chracters
       
-charModule.controller('charactersController', ['Character', function(Character) {
-   this.char1 = new Character();
+charModule.controller('charactersController', ['charactersService', function(charactersService) {
+   this.char1 = charactersService.firstCharacter;
 }]);
 
 charModule.factory('Character', [function() {
    var Character = function() {};
    return Character;
+}]);
+
+charModule.service('charactersService', ['Character', function(Character) {
+   this.firstCharacter = new Character();
 }]);
