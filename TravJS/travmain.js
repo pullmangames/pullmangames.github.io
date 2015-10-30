db91ca48db107d2b6854083bdb4b37824b65d475
Justin <justin@localhost>
2015-10-29 23:37:43 -0700

    removed old demo code

--- TravJS/travmain.js
+++ TravJS/travmain.js
@@ -28,33 +28,3 @@ var diff = {	simple:6,
 				verydifficult:-4,
 				formidable: -6
 			}
-
-/*
-app.controller('TodoListController', function() { //declare a controller
-        var todoList = this;    					//reference to self (module?)
-        todoList.todos = [							//local var for initial list
-          {text:'learn angular', done:true},		//...
-          {text:'build an angular app', done:false}];//...
-     
-        todoList.addTodo = function() {    			//declare handler for add button
-          todoList.todos.push({text:todoList.todoText, done:false}); //push new item from form to list
-          todoList.todoText = '';					//clear web form todo item
-        };
-     
-        todoList.remaining = function() {			//declare function for counting items
-          var count = 0;							//duh
-          angular.forEach(todoList.todos, function(todo) {	//execute new function for each item in the list
-            count += todo.done ? 0 : 1;				// ternary operator, add one to the count if the thing isn't done
-          });										//end of "for loop" body
-          return count;								//returns value from remaining function
-        };
-     
-        todoList.archive = function() {				//declare handler for archive function
-          var oldTodos = todoList.todos;			//copy the todolist for evaluation since we'll be editing
-          todoList.todos = [];						//clear the main todolist
-          angular.forEach(oldTodos, function(todo) { //define "for loop" function to iterate oldtodolist
-            if (!todo.done) todoList.todos.push(todo);  // if the old item is not done, add it to the new list
-          });
-        };
-      })
-    */
