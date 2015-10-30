77cff87466b14a3e3a33a103f2e9d6de640e5732
Justin <justin@localhost>
2015-10-29 23:34:00 -0700

    moved dice and shipman to separate files
    changed from .view in main html to separate .html files

--- TravJS/travmain.js
+++ TravJS/travmain.js
@@ -3,9 +3,9 @@ app = angular.module('TravApp', ['ngRoute', 'travellerCharacters', 'diceRoller',
       
 app.config(function ($routeProvider) {
    $routeProvider
-      .when("/roll",       {controller: "RollController as roller",           templateUrl: "roll.view"})
+      .when("/roll",       {controller: "RollController as roller",           templateUrl: "roll.html"})
       .when("/characters", {controller: "charactersController as characters", templateUrl: "characters.view"})
-      .when("/shipManagement", {controller: "shipManagementController as shipMan", templateUrl: "shipMan.view"})
+      .when("/shipManagement", {controller: "shipManagementController as shipMan", templateUrl: "shipMan.html"})
       .otherwise({redirectTo: "/roll"});
 });
 
