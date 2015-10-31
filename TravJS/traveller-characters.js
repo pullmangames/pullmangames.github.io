f536a8e6a89f95442ef1632cadb26ac7a8ebc3ce
Jared August <jaugust@eecs.wsu.edu>
2015-10-31 12:40:49 -0700

    Support multiple characters. Still need to deal with issue of 0 characters.

--- TravJS/traveller-characters.js
+++ TravJS/traveller-characters.js
@@ -1,12 +1,36 @@
 charModule = angular.module('travellerCharacters', []); //declare the module for handling chracters
       
 charModule.controller('charactersController', ['$scope', 'charactersService', function($scope, charactersService) {
-   $scope.char1 = charactersService.firstCharacter;
-   $scope.skillList = charactersService.firstCharacter.skills.fullList;
+   $scope.characterList = charactersService.characters;
+
    $scope.setSkillFocus = function(name)
    {
       window.setTimeout(function() {document.getElementById(name).focus()}, 0);
    };
+
+   $scope.selectCharacter = function(id)
+   {
+      $scope.selectedCharacter = $scope.characterList[id];
+      $scope.skillList = $scope.selectedCharacter.skills.fullList;
+   }
+   
+   $scope.addCharacter = function()
+   {
+      $scope.selectCharacter(charactersService.addCharacter());
+   }
+   
+   $scope.deleteCharacter = function(id)
+   {
+      charactersService.deleteCharacter(id);
+      $scope.selectCharacter(0);
+   }
+   
+   $scope.isActive = function(id)
+   {
+      return (id === $scope.selectedCharacter.id);
+   }
+   
+   $scope.addCharacter();
 }]);
 
 charModule.factory('character', ['skills', function(skills) {
@@ -17,5 +41,30 @@ charModule.factory('character', ['skills', function(skills) {
 }]);
 
 charModule.service('charactersService', ['character', function(character) {
-   this.firstCharacter = new character();
+   this.characters = [];
+   _newCharsCreated = 0;
+   _recalcIds = function(arrayToRecalc)
+   {
+      len = arrayToRecalc.length
+      for (var i = 0; i < len; i++)
+      {
+         arrayToRecalc[i].id = i;
+      }
+   };
+
+   this.addCharacter = function()
+   {
+      var newChar = new character();
+      _newCharsCreated++;
+      newChar.name = "New Character #" + _newCharsCreated;
+      var id = this.characters.push(newChar) - 1;
+      newChar.id = id;
+      return id;
+   };
+   
+   this.deleteCharacter = function(id)
+   {
+      this.characters.splice(id, 1);
+      _recalcIds(this.characters);
+   };
 }]);
