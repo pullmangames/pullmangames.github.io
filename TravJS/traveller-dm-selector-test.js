09218c99450f21f34a88c42192ff79cdeb282b2b
Jared <augustjp78@hotmail.com>
2015-11-15 19:42:00 -0800

    Moved trav-skill-check-dm into diceRoller

--- TravJS/traveller-dm-selector-test.js
+++
@@ -1,5 +0,0 @@
-dmSelectorTestModule = angular.module('travellerDmSelectorTest', []); //declare the module for handling alerts
-
-dmSelectorTestModule.controller('dmSelectorTestController', ['$scope', 'skillsService', function($scope, skillsService) {
-   $scope.dmResult = {};
-}]);
--- TravJS/travmain.js
+++ TravJS/travmain.js
@@ -1,10 +1,9 @@
-app = angular.module('TravApp', ['ngRoute', 'ngSanitize', 'ui.bootstrap', 'ui.select', 'travellerAlerts', 'travellerCharacters', 'travellerSkills', 'travellerDmSelectorTest', 'diceRoller', 'shipManagement', 'randomModule', 'decoder']) //declare the main module
+app = angular.module('TravApp', ['ngRoute', 'ngSanitize', 'ui.bootstrap', 'ui.select', 'travellerAlerts', 'travellerCharacters', 'travellerSkills', 'diceRoller', 'shipManagement', 'randomModule', 'decoder']) //declare the main module
       
 app.config(function ($routeProvider) {
    $routeProvider
       .when("/roll",           {controller: "RollController as roller",                 templateUrl: "roll.view"})
       .when("/characters",     {controller: "charactersController as characters",       templateUrl: "characters.view"})
-      .when("/dmSelectorTest", {controller: "dmSelectorTestController as dmSelectTest", templateUrl: "dmSelectorTest.view"})
       .when("/shipManagement", {controller: "shipManagementController as shipMan",      templateUrl: "shipMan.view"})
       .when("/random",         {controller: "randomController as tables",               templateUrl: "randomtables.view"})
       .when("/decoder",        {controller: "decoderController as decoder",             templateUrl: "decoder.view"})
