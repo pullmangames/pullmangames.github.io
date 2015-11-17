935763f389decf183a8b2cdd8e319d0468a36931
Justin <justin@localhost>
2015-11-16 16:54:05 -0800

    Logs and status working
    Manual add log, cash, cargo, all working.  Status stuff mostly working.

--- TravJS/ship-management.js
+++ TravJS/ship-management.js
@@ -1,26 +1,95 @@
-shipManModule = angular.module('shipManagement', []); //declare the module for handling chracters
+shipManModule = angular.module('shipManagement', []); //declare the module for managing the ship
       
 shipManModule.controller('shipManagementController', function ($scope) {
 	var smm=this;
 	
+	var theShip={}
+	theShip.size=200;
+	theShip.maxfuel=44;
+	theShip.maxcargo=64;
+	theShip.maxpassengers=5;
+	theShip.maxlow=6;
+	theShip.crew=10;
+	theShip.computer=5;
+	theShip.Jump=2;
+	theShip.fuelprocessors=2;
+	theShip.streamlined=true;
+	theShip.staterooms=10;
+	theShip.newprice=99999999;
+	theShip.discounted=.6;
+	theShip.principal=theShip.newprice*theShip.discounted;
+	theShip.monthlypayment=theShip.principal/240;
+	theShip.annualmaint=theShip.newprice*.001;
+	
+	
+	smm.partyShip=theShip;
+		
+	smm.log=[];
+	smm.log.status={};
+	smm.log.status.today=001;
+	smm.log.status.year=1105;
+	smm.log.status.fuel=0;
+	smm.log.status.cargo=[{"type":"demo", "detail":"some deets", "tons":10},{"type":"demo2", "detail":"some deets", "tons":20}] //todo remove demo cargo
+	smm.log.status.midpass=0;
+	smm.log.status.highpass=0;
+	smm.log.status.lowpass=0;
+	smm.log.status.cash=0;
+	smm.log.status.maintFund=0;
+	smm.log.status.totalCargo = function(){
+		var total=0;
+		for (var i=0;i<this.cargo.length;i++)
+		{
+			total+=this.cargo[i].tons;
+		}
+		return total;
+	};
+	
+	
+	smm.log.entries=[];
+	
+	smm.inputYear=smm.log.status.year;
+	smm.inputDate=smm.log.status.today;
+	
+	
+	//cargo on the ship looks like this:
+	//type, detail, tons, price paid, Risk DM, Danger DM, purchase DM, sale DM,
+	
+	//cargo for sale also has max risk dm, base price, max tons
+	
+	
 	smm.manualLog=function(){
-		var newentry={};
-		newentry.text=smm.input;
-		newentry.start=smm.inputDate;
-		newentry.elapsed=smm.inputTimeElapsed;
-		smm.logEntry(newentry);
-		
+		smm.logEntry(smm.inputYear,smm.inputDate,smm.inputTimeElapsed,smm.logText);
 	};
 	
-	
-	smm.logEntry=function(newEntry) {
-		
-		alert("ENTERED"+newEntry);
-		
-	};
 
+	smm.manualCargoAdd=function(){
+		var newcargo=cargoFactory();
+		newcargo.type=smm.addCargoType;
+		newcargo.tons=smm.addCargoTons;
+		smm.log.status.cargo.push(newcargo);
+		smm.logEntry(smm.log.status.year, smm.log.status.today, smm.inputTimeElapsed, "[manual] loaded " + smm.addCargoType + ": "  +smm.addCargoTons + " dT");
+	};
+	
+	smm.manualCredits=function(){
+	smm.log.status.cash += smm.addMoney;
+	smm.logEntry(smm.log.status.year, smm.log.status.today, smm.inputTimeElapsed, "[manual] " + smm.addMoneyDesc + ": " + smm.addMoney + " = " +smm.log.status.cash);
+	}
+	
+	smm.logEntry=function(newyear, newdate, newelapsed, newtext){
+		smm.logEntryRaw({"year":newyear, "startDate":newdate, "elapsed":newelapsed, "text":newtext});
+		};
+
+	smm.logEntryRaw=function(newEntry) {
+		smm.log.push(newEntry)
+	};
 
 });
+
+
+
+var cargoFactory = function(){
+	return {"type":"", "detail":"", "tons":0};
+}
 
 
 
